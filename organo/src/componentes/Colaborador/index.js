6dc65d940e711c72fb706b9297791c1911be0c85
Victor Barra <victorbarrapereira@gmail.com>
2023-08-09 09:37:46 -0300

    aula 1 - pasta e arquivos corrigidos

--- organo/src/componentes/Colaborador/index.js
+++ organo/src/componentes/Colaborador/index.js
@@ -1,18 +1,15 @@
-import './Colaborador.css'
+import './colaborador.css'
 
-const Colaborador = ({nome, imagem, cargo, corDeFundo}) => {
-    return (
-        <div className='colaborador'>
-            <div className='cabecalho' style={{backgroundColor:corDeFundo}}>
-                <img src={imagem} alt={nome}/>        
-            </div>
-            <div className='rodape'>
-                <h4>{nome}</h4>
-                <h5>{cargo}</h5>
-            </div>
+const Colaborador = ({ colaborador, corDeFundo }) => {
+    return (<div className="colaborador">
+        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+            <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
-    )
+        <div className="rodape">
+            <h4>{colaborador.nome}</h4>
+            <h5>{colaborador.cargo}</h5>
+        </div>
+    </div>)
 }
 
 export default Colaborador
-
--- organo/src/componentes/Rodape/index.js
+++ organo/src/componentes/Rodape/index.js
@@ -1,36 +1,36 @@
-import './Rodape.css'
+
+import './rodape.css'
 
 const Rodape = () => {
     return (<footer className="footer">
-        <section>
-            <ul>
-                <li>
-                    <a href="instagram.com" target="_blank" >
-                        <img src="/../imagens/ig.png" alt="icone do instagram" />
-                    </a>
-                </li>
-                <li>
-                    <a href="twitter.com" target="_blank" >
-                        <img src="/../imagens/tw.png" alt="icone do twitter" />
-                    </a>
-                </li>
-                <li>
-                    <a href="facebook.com" target="_blank" >
-                        <img src='/../imagens/fb.png' alt="icone do facebook" />
-                    </a> 
-                </li>
-            </ul>
-        </section>
-        <section>
-            <img src="/../imagens/logo.png" alt="logo" />
-        </section>
-        <section>
-            <p>
-                Desenvolvido por Alura.
-            </p>
-        </section>
+       <section>
+       <ul>
+            <li>
+                <a href="facebook.com" target="_blank">
+                    <img src="/imagens/facebook.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="twitter.com" target="_blank">
+                    <img src="/imagens/twitter.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="instagram.com" target="_blank">
+                    <img src="/imagens/instagram.png" alt="" />
+                </a>
+            </li>
+        </ul>
+       </section>
+       <section>
+       <img src="/imagens/logo.png" alt="" />
+       </section>
+       <section>
+        <p>
+            Desenvolvido por Alura.
+        </p>
+       </section>
     </footer>)
 }
 
 export default Rodape
-
